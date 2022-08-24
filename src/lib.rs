235ae84bc182b95378c4702cad774f7fd06a42be
SigTen <mtucksen@gmail.com>
2022-08-24 09:14:16 -0500

    add game states, add empty asset folder

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,21 @@ use bevy::prelude::*;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum GameState {
+    Init,
+    Menu,
+    Credits,
+    LoadGame,
+    PlayGame,
+    GameEnd,
+}
+
 pub struct CardinalCreatorPlugin;
 
 impl Plugin for CardinalCreatorPlugin {
     fn build(&self, app: &mut App) {
-
+        app.add_state(GameState::Init);
         #[cfg(debug_assertions)]
         {
             app.add_plugin(FrameTimeDiagnosticsPlugin::default())
